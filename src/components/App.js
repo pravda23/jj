5ead97dde8294c36a6437cb1d4e78d6f7dcb6c13
pravda23 <pravda23@gmail.com>
2023-02-07 14:03:15 +0200

    todo visuals component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import "../styles/App.styles.scss";
 import { useState } from "react";
 import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 import About from "./About.component";
-import Contact from "./Contact.component";
+import Footer from "./Footer.component";
 import Visuals from "./Visuals.component";
 import Navbar from "./Navbar.component";
-import Header from "./Header.component";
 
 function App() {
   const baseUrl = "https://johnbartmann.com/getjohnnyjazz";
@@ -20,22 +19,47 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <video className="video" src={vidUrl} autoPlay loop muted={videoMuted} />
-      <div className="navbar-container">
-        <Navbar />
-        <div onClick={setVolumeOnClick} className="player-icon">
-          {volumeIcon ? (
-            <FaVolumeUp size="2rem" />
-          ) : (
-            <FaVolumeMute size="2rem" />
-          )}
-        </div>
+    <div>
+      <div className="app-container">
+        <video
+          className="video"
+          src={vidUrl}
+          autoPlay
+          loop
+          muted={videoMuted}
+        />
+        <div className="header-container"></div>
+        <BrowserRouter>
+          {/* <Navbar /> */}
+          <div className="navbar">
+            <Link to="/">
+              <h1 className="btn btn-outline-warning p-2 m-2 btn-lg btn-bold">
+                ABOUT
+              </h1>
+            </Link>
+            <Link to="/visuals">
+              {" "}
+              <h1 className="btn btn-outline-warning p-2 m-2 btn-lg btn-bold">
+                VISUALS
+              </h1>
+            </Link>
+            <div onClick={setVolumeOnClick} className="player-icon">
+              {volumeIcon ? (
+                <FaVolumeUp size="2rem" />
+              ) : (
+                <FaVolumeMute size="2rem" />
+              )}
+            </div>
+          </div>
+          <Routes>
+            <Route index element={<About />} />
+            <Route path="/visuals" element={<Visuals />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-
-      <Header />
-      <About />
-      <Contact />
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -1,9 +1,7 @@
-const Navbar = () => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
-};
+import "../styles/App.styles.scss";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+const Navbar = () => {
+  return <>NAVBAR</>;
+};
 export default Navbar;
